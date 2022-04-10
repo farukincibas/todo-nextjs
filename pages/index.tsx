ea7c82afc14bb6e89c669c3ebd2d61365f84b68b
omer faruk incibas <farukincibas@hotmail.com>
2022-04-10 13:30:00 +0300

    imp(project): initial

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <span>hello world</span>
+      </main>
+
+      <footer className={styles.footer}>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home
